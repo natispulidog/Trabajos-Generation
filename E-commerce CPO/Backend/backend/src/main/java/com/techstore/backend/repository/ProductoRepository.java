d7dba8208f783150a033ec375d0c76b8888dc5f1
natispulidog <angie.pulido@genstudents.org>
2026-05-12 00:16:48 -0500

    Delete E-commerce CPO directory

--- E-commerce CPO/Backend/backend/src/main/java/com/techstore/backend/repository/ProductoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.techstore.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.techstore.backend.models.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {}
--- E-commerce CPO/Backend/backend/src/main/java/com/techstore/backend/repository/VisitaFincaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.techstore.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.techstore.backend.models.VisitaFinca;
-
-public interface VisitaFincaRepository extends JpaRepository<VisitaFinca, Long> {}
